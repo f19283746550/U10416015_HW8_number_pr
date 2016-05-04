cf3190764760f3be8a416396a5e66c59b7d6c27f
f19283746550 <f19283746550@yahoo.com.tw>
2016-05-04 22:18:19 +0800

    Update Number_Pr.java
    加註解

--- Number_Pr.java
+++ Number_Pr.java
@@ -10,18 +10,18 @@ public class Number_Pr {
 		System.out.println("input:1.Plus 2.Minus 3.Multiply 4.Divide");
 		switch(input.nextInt()) {
 			case 1:
-				System.out.println(x.add(y));
+				System.out.println(x.add(y));//加起來後印出來
 				break;
 			case 2:
-				System.out.println(x.subtract(y));
+				System.out.println(x.subtract(y));//減掉後印出來
 				break;
 			case 3:
-				System.out.println(x.multiply(y));
+				System.out.println(x.multiply(y));//乘起來後印出來
 				break;
 			case 4:
-				System.out.println("The ? digit after the decimal point.");
+				System.out.println("The ? digit after the decimal point.");//小數點後幾位數
 				int a=input.nextInt();
-				System.out.println(x.divide(y,a,BigDecimal.ROUND_HALF_UP));
+				System.out.println(x.divide(y,a,BigDecimal.ROUND_HALF_UP));//除後印出來
 				break;
 		}
 	}
